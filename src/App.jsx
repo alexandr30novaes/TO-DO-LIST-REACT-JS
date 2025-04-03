33f50e12ef1bc7fee2facbcfe1890e004f3df998
Alexandre Novaes De Oliveira <ale.oliveira00@gmail.com>
2025-04-02 21:46:49 -0300

    style

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
+import { useState, useEffect } from "react"
+
+
 import TaskInput from "./components/TaskInput"
 import TaskList from "./components/TaskList"
 
 function App() {
+
+
+
   return (
     <>
-      <h1>Lista de Tarefas</h1>
-      <TaskInput />
-      <TaskList />
+      <div className="container">
+        <div className="logo">
+          <img src="/logo-list.png" alt="Logo" />
+          <h1>Lista de Tarefas</h1>
+        </div>
+        <div className="line">
+          <div className="container-line">
+            <TaskInput />
+            <TaskList />
+          </div>
+        </div>
+      </div>
     </>
   )
 }
